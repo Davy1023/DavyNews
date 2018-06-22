5adedd52e21b7624cdb116b27c252621b4749d8c
Davy1023 <you@example.com>
2018-06-22 15:37:30 +0800

    commit

---
+++ app/src/main/java/com/davy/davy_news/ui/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.davy.davy_news.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
