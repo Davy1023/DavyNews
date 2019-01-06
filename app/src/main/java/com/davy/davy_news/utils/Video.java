708bcc71bbbfc7655546f8f2a1d98cb73981a4c3
Davy <davy.zhu@zdw.com>
2019-01-06 19:47:44 +0800

    he

--- app/src/main/java/com/davy/davy_news/utils/Video.java
+++ app/src/main/java/com/davy/davy_news/utils/Video.java
@@ -1,9 +1,16 @@
 package com.davy.davy_news.utils;
 
+import com.davy.davy_news.bean.Channel;
+
+import java.util.List;
+
 /**
  * author: Davy
  * date: 19/1/6
  */
 public class Video {
 
+    public List<Channel> selecteDatas;
+    public List<Channel> unSelecteDatas;
+    public List<Channel> allChannels;
 }
